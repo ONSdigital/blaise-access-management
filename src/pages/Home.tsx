f06f8b16adadcde3f9fc8ed16fbb0a03080b021d
Sam Bryant <srbry@hotmail.com>
2022-02-09 11:08:02 +0000

    Update deps

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,20 +41,6 @@ function Home({ user }: Props): ReactElement {
                                 </ul>
                             </div>
                         </div>
-                        {/*<div className="grid__col col-4@m">*/}
-                        {/*    <div className="card" aria-labelledby="title3" aria-describedby="text3">*/}
-                        {/*        <h2 className="u-fs-m" id="title3">*/}
-                        {/*            <a href="#0">Your data and security</a>*/}
-                        {/*        </h2>*/}
-                        {/*        <p id="text3">How we keep your data safe and what happens to your personal information.</p>*/}
-                        {/*        <ul className="list list--dashed">*/}
-                        {/*            <li className="list__item ">*/}
-                        {/*                <a href="#0" className="list__link ">List item 1</a></li>*/}
-                        {/*            <li className="list__item ">*/}
-                        {/*                <a href="#0" className="list__link ">List item 2</a></li>*/}
-                        {/*        </ul>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
                     </div>
                 </div>
             </main>
