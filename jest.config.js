c6f788497c50d1e441528084ed8fe0b1b5107866
kristian4res <57638182+kristian4res@users.noreply.github.com>
2024-04-03 18:21:32 +0100

    fix???

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 process.env = Object.assign(process.env, {
-    BLAISE_API_URL: "mock",
+    BLAISE_API_URL: "http://mock",
     PROJECT_ID: "mock-project",
     SERVER_PARK: "mock-server-park"
 });
