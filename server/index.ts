6e3930152d558fd9b06e344588715007d8ce5084
am-ons <171134774+am-ons@users.noreply.github.com>
2024-06-20 10:55:36 +0100

    refactor: Protected server API endpoint test passes successfully

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,16 @@
-import app from "./server";
+import GetNodeServer from "./server";
 import pino from "pino";
+import { loadConfigFromEnv } from "./Config";
+import BlaiseApiClient from "blaise-api-node-client";
+import { Auth } from "blaise-login-react/blaise-login-react-server";
 
 const port: string = process.env.PORT || "5002";
 const logger = pino();
-app.listen(port);
+
+const config = loadConfigFromEnv();
+const blaiseApiClient = new BlaiseApiClient(config.BlaiseApiUrl);
+const auth = new Auth(config);
+const server = GetNodeServer(config, blaiseApiClient, auth);
+server.listen(port);
 
 logger.info("App is listening on port " + port);
--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -2,19 +2,30 @@
  * @jest-environment node
  */
 
-import app from "../server"; // Link to your server file
 import supertest from "supertest";
+import GetNodeServer from "../server";
+import { loadConfigFromEnv } from "../Config";
+import BlaiseApiClient, {NewUser, User} from "blaise-api-node-client";
+import { Auth } from "blaise-login-react/blaise-login-react-server";
+import { IMock, Mock, It, Times  } from "typemoq";
 
 // Temporary fix for Jest open handle issue (gcp profiler TCPWRAP error)
 jest.mock("@google-cloud/profiler", () => ({
     start: jest.fn().mockReturnValue(Promise.resolve())
 }));
 
-const request = supertest(app);
+const config = loadConfigFromEnv();
+const blaiseApiMock: IMock<BlaiseApiClient> = Mock.ofType(BlaiseApiClient);
+Auth.prototype.ValidateToken = jest.fn().mockReturnValue(true);
+const auth = new Auth(config);
+const server = GetNodeServer(config, blaiseApiMock.object, auth);
+const sut = supertest(server);
+
 
 describe("Test Heath Endpoint", () => {
+    
     it("should return a 200 status and json message", async () => {
-        const response = await request.get("/bam-ui/version/health");
+        const response = await sut.get("/bam-ui/version/health");
 
         expect(response.statusCode).toEqual(200);
         expect(response.body).toStrictEqual({ healthy: true });
@@ -22,6 +33,7 @@ describe("Test Heath Endpoint", () => {
 });
 
 describe("app engine start", () => {
+    
     it("should return a 200 status and json message", async () => {
         process.env = Object.assign({
             PROJECT_ID: "mock",
@@ -30,8 +42,52 @@ describe("app engine start", () => {
             SESSION_TIMEOUT: "12h"
         });
 
-        const response = await request.get("/_ah/start");
+        const response = await sut.get("/_ah/start");
 
         expect(response.statusCode).toEqual(200);
+    });
+});
+
+import role_to_serverparks_map from '../role-to-serverparks-map.json'
+import { size } from "lodash";
+describe("Test /api/users POST createUser with correct server parks", () => {
+    beforeEach(() => {
+        blaiseApiMock.reset();
+    });
+
+    afterAll(() => {
+        blaiseApiMock.reset();
+    });
+
+    it("should call Blaise API createUser endpoint with correct serverParks for each role in server/role-to-serverparks-map.json and return http status 200", async () => {
+        let index = 0;
+        let roleCount = size(role_to_serverparks_map);
+        for(let roleName in role_to_serverparks_map)   
+        {
+            blaiseApiMock.reset();
+            console.log("Running for role %i of %i:  %s", ++index, roleCount, roleName);
+            
+            const spmap = role_to_serverparks_map[roleName];
+            const newUser : NewUser = {
+                name:  "name1",
+                password: "password1",
+                role: roleName,
+                serverParks: spmap,
+                defaultServerPark: spmap[0]
+            };
+            blaiseApiMock.setup((api) => api.createUser(It.isAny())).returns(async () => newUser);
+            
+            const response = await sut.post("/api/users")
+                .field("role", roleName);
+
+            expect(response.statusCode).toEqual(200);
+            blaiseApiMock.verify(a => a.createUser(It.is<NewUser>(
+                x=> x.defaultServerPark == newUser.defaultServerPark 
+                    && x.role == newUser.role
+                    && Array.isArray(x.serverParks) && x.serverParks.every(item => typeof item === "string")
+                    && x.serverParks.every((val, idx) => val === newUser.serverParks[idx])
+            )), Times.exactly(1));
+            expect(response.body).toStrictEqual(newUser);
+        }
     });
 });
