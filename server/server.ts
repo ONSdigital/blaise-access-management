a2d7dbc3eeb593675cf38f39596283f1eb6cbbd9
am-ons <171134774+am-ons@users.noreply.github.com>
2024-06-21 16:08:16 +0100

    BLAIS5-4274 increase unit test coverage in bam (#356)
    * chore: fail_ci_if_error: true
    
    * refactor: Protected server API endpoint test passes successfully
    
    * feat: Add all tests to cover the code in /api/users POST endpoint
    
    * fix: Prevent disk I/O each time GET * request hits server
    
    * fix: Fix formatting: yarn eslint . --fix
    fix: Add typemoq as dependency
    
    * feat: Add all tests to cover the code in /api/users DELETE endpoint
    
    * feat: Add all tests to cover the code in /api/users GET endpoint
    
    * feat: Add all tests to cover the code in /api/roles GET endpoint
    
    * feat: Add all tests to cover the code in /api/change_password/:user GET endpoint
    
    * fix: Fix Linting with ESLINT: yarn eslint . --fix
    
    * fix: Load .env variables if current env is not PROD.
    
    * fix: Remove launch.json from source control as it should be dev local file.
    
    * feat: Add test for "external Blaise API changePassword endpoint rejects request with error message"
    
    * chore: No changes in yarn.lock file.
    
    * refactor: Removed redundant json map from client. We don't trust client side script, mapping happens on server side so only one json map file needed that is on the server side.

--- server/server.ts
+++ server/server.ts
@@ -2,71 +2,71 @@ import express, { Request, Response } from "express";
 import axios from "axios";
 import path from "path";
 import ejs from "ejs";
-import dotenv from "dotenv";
-import { loadConfigFromEnv } from "./Config";
 import createLogger from "./pino";
 import BlaiseAPIRouter from "./BlaiseAPI";
 import multer from "multer";
 import * as profiler from "@google-cloud/profiler";
-import BlaiseApiClient from "blaise-api-node-client";
 import { newLoginHandler, Auth } from "blaise-login-react/blaise-login-react-server";
 import pino from "pino";
+import { CustomConfig } from "./interfaces/server";
+import BlaiseApi from "blaise-api-node-client";
+import { Express } from "express";
+import fs from "fs";
 
-const pinoLogger = pino();
-profiler.start({ logLevel: 4 }).catch((err: unknown) => {
-    pinoLogger.error(`Failed to start profiler: ${err}`);
-});
+export default function GetNodeServer(config: CustomConfig, blaiseApi: BlaiseApi, auth: Auth): Express
+{
+    const pinoLogger = pino();
+    profiler.start({ logLevel: 4 }).catch((err: unknown) => {
+        pinoLogger.error(`Failed to start profiler: ${err}`);
+    });
 
-const upload = multer();
+    const upload = multer();
 
-const server = express();
+    const server = express();
 
-server.use(upload.any());
+    server.use(upload.any());
 
-axios.defaults.timeout = 10000;
+    axios.defaults.timeout = 10000;
 
-const logger = createLogger();
-server.use(logger);
+    const logger = createLogger();
+    server.use(logger);
 
-if (process.env.NODE_ENV !== "production") {
-    dotenv.config();
+    // where ever the react built package is
+    const buildFolder = "../build";
+
+    const loginHandler = newLoginHandler(auth, blaiseApi);
+
+    // Health Check endpoint
+    server.get("/bam-ui/:version/health", async function (req: Request, res: Response) {
+        pinoLogger.info("Heath Check endpoint called");
+        res.status(200).json({ healthy: true });
+    });
+
+    server.use("/", loginHandler);
+
+    // All Endpoints calling the Blaise API
+    server.use("/", BlaiseAPIRouter(config, auth, blaiseApi));
+
+    // treat the index.html as a template and substitute the values at runtime
+    server.set("views", path.join(__dirname, "/views"));
+    server.engine("html", ejs.renderFile);
+    server.use(
+        "/static",
+        express.static(path.join(__dirname, `${buildFolder}/static`))
+    );
+
+    let indexFilePath = path.join(__dirname, `${buildFolder}/index.html`);
+    if (!fs.existsSync(indexFilePath)) {
+        indexFilePath = path.join(__dirname, "../public/index.html");
+    }
+    server.get("*", function (_req: Request, res: Response) {
+        res.render(indexFilePath);
+    });
+
+    server.use(function (err: Error, _req: Request, res: Response) {
+        console.error(err.stack);
+        res.render("../views/500.html", {});
+    });
+
+    return server;
 }
-
-// where ever the react built package is
-const buildFolder = "../build";
-
-// load the .env variables in the server
-const config = loadConfigFromEnv();
-
-const auth = new Auth(config);
-const blaiseApiClient = new BlaiseApiClient(config.BlaiseApiUrl);
-const loginHandler = newLoginHandler(auth, blaiseApiClient);
-
-// Health Check endpoint
-server.get("/bam-ui/:version/health", async function (req: Request, res: Response) {
-    pinoLogger.info("Heath Check endpoint called");
-    res.status(200).json({ healthy: true });
-});
-
-server.use("/", loginHandler);
-
-// All Endpoints calling the Blaise API
-server.use("/", BlaiseAPIRouter(config, auth, blaiseApiClient));
-
-// treat the index.html as a template and substitute the values at runtime
-server.set("views", path.join(__dirname, "/views"));
-server.engine("html", ejs.renderFile);
-server.use(
-    "/static",
-    express.static(path.join(__dirname, `${buildFolder}/static`))
-);
-
-server.get("*", function (_req: Request, res: Response) {
-    res.render(path.join(__dirname, `${buildFolder}/index.html`));
-});
-
-server.use(function (err: Error, _req: Request, res: Response) {
-    console.error(err.stack);
-    res.render("../views/500.html", {});
-});
-export default server;
