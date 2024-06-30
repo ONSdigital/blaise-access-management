3b6390c999364d983c34a70529eae4ae8dc076e9
kristian4res <57638182+kristian4res@users.noreply.github.com>
2024-06-30 20:02:35 +0100

    refactor: rename utilities into api

---
+++ src/api/http/roles.test.ts
@@ -0,0 +1,106 @@
+import { cleanup } from "@testing-library/react";
+import { mock_server_request_function, mock_server_request_Return_JSON } from "../../tests/utils";
+import { addNewRole, getAllRoles } from "./roles";
+import { UserRole } from "blaise-api-node-client";
+
+const roleList: UserRole[] = [
+    { name: "DST", permissions: ["Admin", "Bacon.access"], description: "A role" },
+    { name: "BDSS", permissions: ["Admin"], description: "Another role" }
+];
+
+describe("Function getAllRoles() ", () => {
+
+    it("It should return true with data if the list is returned successfully", async () => {
+        mock_server_request_Return_JSON(200, roleList);
+        const [success, roles] = await getAllRoles();
+        expect(success).toBeTruthy();
+        expect(roles).toEqual(roleList);
+    });
+
+    it("It should return true with an empty list if a 404 is returned from the server", async () => {
+        mock_server_request_Return_JSON(404, []);
+        const [success, roles] = await getAllRoles();
+        expect(success).toBeTruthy();
+        expect(roles).toEqual([]);
+    });
+
+    it("It should return false with an empty list if request returns an error code", async () => {
+        mock_server_request_Return_JSON(500, {});
+        const [success, roles] = await getAllRoles();
+        expect(success).toBeFalsy();
+        expect(roles).toEqual([]);
+    });
+
+    it("It should return false with an empty list if request JSON is not a list", async () => {
+        mock_server_request_function(jest.fn(() => Promise.resolve({
+            status: 400,
+            json: () => Promise.reject("Failed")
+        })));
+
+        const [success, roles] = await getAllRoles();
+        expect(success).toBeFalsy();
+        expect(roles).toEqual([]);
+    });
+
+    it("It should return false with an empty list if request JSON is invalid", async () => {
+        mock_server_request_Return_JSON(200, { name: "NAME" });
+        const [success, roles] = await getAllRoles();
+        expect(success).toBeFalsy();
+        expect(roles).toEqual([]);
+    });
+
+    it("It should return false with an empty list if request call fails", async () => {
+        mock_server_request_function(jest.fn(() => {
+            throw new Error("Network error");
+        }));
+
+        const [success, roles] = await getAllRoles();
+        expect(success).toBeFalsy();
+        expect(roles).toEqual([]);
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        cleanup();
+    });
+});
+
+describe("Function addNewRole(user: User) ", () => {
+    const newRole: UserRole = {
+        permissions: [],
+        name: "New Role",
+        description: "This is a new role"
+    };
+
+    it("It should return true if the role has been created successfully", async () => {
+        mock_server_request_Return_JSON(201, {});
+        const success = await addNewRole(newRole);
+        expect(success).toBeTruthy();
+    });
+
+    it("It should return false if a 404 is returned from the server", async () => {
+        mock_server_request_Return_JSON(404, []);
+        const success = await addNewRole(newRole);
+        expect(success).toBeFalsy();
+    });
+
+    it("It should return false if request returns an error code", async () => {
+        mock_server_request_Return_JSON(500, {});
+        const success = await addNewRole(newRole);
+        expect(success).toBeFalsy();
+    });
+
+    it("It should return false if request call fails", async () => {
+        mock_server_request_function(jest.fn(() => {
+            throw new Error("Network error");
+        }));
+
+        const success = await addNewRole(newRole);
+        expect(success).toBeFalsy();
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        cleanup();
+    });
+});
---
+++ src/api/http/roles.ts
@@ -0,0 +1,37 @@
+import { requestPromiseJson, requestPromiseJsonList } from "./requestPromise";
+import { UserRole } from "blaise-api-node-client";
+
+type getRolesListResponse = [boolean, UserRole[]];
+
+async function getAllRoles(): Promise<getRolesListResponse> {
+    try {
+        const url = "/api/roles";
+        const [success, data] = await requestPromiseJsonList<UserRole>("GET", url);
+        return [success, data] as getRolesListResponse;
+    } catch (error) {
+        return [false, []] as getRolesListResponse;
+    }
+}
+
+function addNewRole(newRole: UserRole): Promise<boolean> {
+    const url = "/api/roles";
+
+    return new Promise((resolve: (object: boolean) => void) => {
+
+        const formData = new FormData();
+        formData.append("name", newRole.name);
+        formData.append("description", newRole.description);
+
+        requestPromiseJson("POST", url, formData).then(([status]) => {
+            if (status === 201) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        }).catch(() => {
+            resolve(false);
+        });
+    });
+}
+
+export { getAllRoles, addNewRole };
