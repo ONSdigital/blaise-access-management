1b308a934675ae43f9c536a934b2eb453f0cd138
kristian4res <57638182+kristian4res@users.noreply.github.com>
2024-11-04 10:52:54 +0000

    refactor: assign correct serverparks based on role, before sending to server

--- src/ClientConfig.ts
+++ src/ClientConfig.ts
@@ -7,7 +7,7 @@ import role_to_serverparks_map_json from "./role-to-serverparks-map.json";
 export function loadConfigFromEnv(): ClientConfig {
     const roleToServerParksMap: { [key: string]: string[] } = role_to_serverparks_map_json;
     return {
-        DefaultServerPark: "gusty",
+        DefaultServerPark: roleToServerParksMap["DEFAULT"][0],
         RoleToServerParksMap: roleToServerParksMap
     };
 }
