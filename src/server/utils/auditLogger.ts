e1f6fc38b17f137ab282912552d1a912430bfe04
Richmond Rice <richmond@ricey.org>
2026-07-17 21:10:34 +0100

    log injection fix

--- src/server/utils/auditLogger.ts
+++ src/server/utils/auditLogger.ts
@@ -59,13 +59,13 @@ export default class AuditLogger {
   info(logger: IncomingMessage["log"], message: string): void {
     const log = formatLogMessage(message, "info");
 
-    logger.info(String(log).replace(/[\r\n]+/g, " ").replace(/[^\x20-\x7E]+/g, ""));
+    logger.info(sanitiseLogText(log));
   }
 
   error(logger: IncomingMessage["log"], message: string): void {
     const log = formatLogMessage(message, "error");
 
-    logger.error(String(log).replace(/[\r\n]+/g, " ").replace(/[^\x20-\x7E]+/g, ""));
+    logger.error(sanitiseLogText(log));
   }
 
   async getLogs(): Promise<AuditLog[]> {
